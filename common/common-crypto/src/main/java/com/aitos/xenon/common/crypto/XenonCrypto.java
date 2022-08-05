ead992f0ca0b3a54df6f7e869dc160461903ef10
hnngm <hnngm@163.com>
2022-08-05 17:38:38 +0800

    根据新需求修改交易逻辑和官网界面ui

---
+++ common/common-crypto/src/main/java/com/aitos/xenon/common/crypto/XenonCrypto.java
@@ -0,0 +1,215 @@
+package com.aitos.xenon.common.crypto;
+
+import com.aitos.xenon.common.crypto.ecdsa.EcdsaKeyPair;
+import com.aitos.xenon.common.crypto.ecdsa.Keccak256Secp256k1;
+import com.aitos.xenon.common.crypto.ed25519.Base58;
+import com.aitos.xenon.common.crypto.ed25519.Ed25519;
+import com.aitos.xenon.common.crypto.ed25519.Ed25519KeyPair;
+import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.KeyAgreement;
+import java.security.*;
+import java.util.Arrays;
+
+@Slf4j
+public class XenonCrypto {
+    public static XenonKeyPair gerateKeyPair(Network network,Algorithm algorithm){
+        XenonKeyPair  xenonKeyPair=new XenonKeyPair();
+        if(algorithm== Algorithm.ED25519){
+            xenonKeyPair.setAlgorithm(algorithm);
+            xenonKeyPair.setNetwork(network);
+
+            Ed25519KeyPair ed25519KeyPair= Ed25519.gerateKeyPair();
+
+            xenonKeyPair.setOriginalPrivateKey(Base58.encode(ed25519KeyPair.getPrivateKey()));
+            xenonKeyPair.setOriginalPublicKey(Base58.encode(ed25519KeyPair.getPublicKey()));
+
+
+            byte[] oldPrivateKeyBytes= ed25519KeyPair.getPrivateKey();
+            byte[] privateKeyBytes=new byte[oldPrivateKeyBytes.length+2];
+
+            privateKeyBytes[0]=(byte)network.getCode();
+            privateKeyBytes[1]=(byte)algorithm.getCode();
+            System.arraycopy(oldPrivateKeyBytes, 0, privateKeyBytes, 2, oldPrivateKeyBytes.length);
+            xenonKeyPair.setPrivateKey(Base58.encode(privateKeyBytes));
+
+            byte[] oldPublicKeyBytes= ed25519KeyPair.getPublicKey();
+            byte[] publicKeyBytes=new byte[oldPublicKeyBytes.length+2];
+
+            publicKeyBytes[0]=(byte)network.getCode();
+            publicKeyBytes[1]=(byte)algorithm.getCode();
+            System.arraycopy(oldPublicKeyBytes, 0, publicKeyBytes, 2, oldPublicKeyBytes.length);
+            xenonKeyPair.setPublicKey(Base58.encode(publicKeyBytes));
+        }else if(algorithm== Algorithm.ECDSA){
+            try{
+                EcdsaKeyPair keyPair = Keccak256Secp256k1.getKeyPair();
+
+                byte[] oldPrivateKeyBytes= keyPair.getPrivateKey();
+                byte[] oldPublicKeyBytes= keyPair.getPublicKey();
+
+                xenonKeyPair.setOriginalPrivateKey(Base58.encode(oldPrivateKeyBytes));
+                xenonKeyPair.setOriginalPublicKey(Base58.encode(oldPublicKeyBytes));
+
+
+
+                byte[] privateKeyBytes=new byte[oldPrivateKeyBytes.length+2];
+
+                privateKeyBytes[0]=(byte)network.getCode();
+                privateKeyBytes[1]=(byte)algorithm.getCode();
+                System.arraycopy(oldPrivateKeyBytes, 0, privateKeyBytes, 2, oldPrivateKeyBytes.length);
+                xenonKeyPair.setPrivateKey(Base58.encode(privateKeyBytes));
+
+                byte[] publicKeyBytes=new byte[oldPublicKeyBytes.length+2];
+
+                publicKeyBytes[0]=(byte)network.getCode();
+                publicKeyBytes[1]=(byte)algorithm.getCode();
+                System.arraycopy(oldPublicKeyBytes, 0, publicKeyBytes, 2, oldPublicKeyBytes.length);
+                xenonKeyPair.setPublicKey(Base58.encode(publicKeyBytes));
+            }catch (Exception e){
+                log.error("gerateKeyPair",e);
+            }
+        }
+        return xenonKeyPair;
+    }
+
+    public static String getPublickKey(String privateKey){
+        XenonKeyPair xenonKeyPair=convertorPrivateKey(privateKey);
+        Network network=xenonKeyPair.getNetwork();
+        Algorithm algorithm=xenonKeyPair.getAlgorithm();
+        if(algorithm.equals(Algorithm.ED25519)){
+            byte[] originalPrivateKeyBytes=Base58.decode(xenonKeyPair.getOriginalPrivateKey());
+            byte[] originalPublicKeyBytes= Ed25519.getPublickKey(originalPrivateKeyBytes);
+            byte[] publicKeyBytes=new byte[originalPublicKeyBytes.length+2];
+            publicKeyBytes[0]=(byte) network.getCode();
+            publicKeyBytes[1]=(byte)algorithm.getCode();
+            System.arraycopy(originalPublicKeyBytes, 0, publicKeyBytes, 2, originalPublicKeyBytes.length);
+            return Base58.encode(publicKeyBytes);
+        }else if(algorithm.equals(Algorithm.ECDSA)){
+
+        }
+        return null;
+    }
+
+    public static String sign(String privateKey,String data){
+        try {
+            XenonKeyPair xenonKeyPair = convertorPrivateKey(privateKey);
+            Network network = xenonKeyPair.getNetwork();
+            Algorithm algorithm = xenonKeyPair.getAlgorithm();
+            if (algorithm.equals(Algorithm.ED25519)) {
+                byte[] originalPrivateKeyBytes = Base58.decode(xenonKeyPair.getOriginalPrivateKey());
+                byte[] originalPublicKeyBytes = Ed25519.sign(originalPrivateKeyBytes, Base58.decode(data));
+                return Base58.encode(originalPublicKeyBytes);
+            } else if (algorithm.equals(Algorithm.ECDSA)) {
+
+            }
+        }catch (Exception e){
+            log.error("sign.error={}",e);
+        }
+        return null;
+    }
+
+    public static String sign(String privateKey,byte[] data){
+        try {
+            XenonKeyPair xenonKeyPair=convertorPrivateKey(privateKey);
+            Network network=xenonKeyPair.getNetwork();
+            Algorithm algorithm=xenonKeyPair.getAlgorithm();
+            if(algorithm.equals(Algorithm.ED25519)){
+                byte[] originalPrivateKeyBytes=Base58.decode(xenonKeyPair.getOriginalPrivateKey());
+                byte[] originalPublicKeyBytes= Ed25519.sign(originalPrivateKeyBytes,data);
+                return Base58.encode(originalPublicKeyBytes);
+            }else if(algorithm.equals(Algorithm.ECDSA)){
+
+            }
+        }catch (Exception e){
+            log.error("sign.error={}",e);
+        }
+        return null;
+    }
+
+    public static boolean verify(String publicKey,byte[] data,String signature){
+        XenonKeyPair xenonKeyPair=convertorPublicKey(publicKey);
+        Network network=xenonKeyPair.getNetwork();
+        Algorithm algorithm=xenonKeyPair.getAlgorithm();
+
+        byte[] signatureBytes=Base58.decode(signature);
+        if(algorithm.equals(Algorithm.ED25519)){
+            byte[] originalPublicKeyBytes=Base58.decode(xenonKeyPair.getOriginalPublicKey());
+            Boolean verify = Ed25519.verify(originalPublicKeyBytes,data,signatureBytes);
+            return verify;
+        }
+        return false;
+    }
+
+    public static boolean verify(String publicKey,String data,String signature){
+        XenonKeyPair xenonKeyPair=convertorPublicKey(publicKey);
+        Network network=xenonKeyPair.getNetwork();
+        Algorithm algorithm=xenonKeyPair.getAlgorithm();
+
+        byte[] dataBytes=Base58.decode(data);
+        byte[] signatureBytes=Base58.decode(signature);
+        if(algorithm.equals(Algorithm.ED25519)){
+            byte[] originalPublicKeyBytes=Base58.decode(xenonKeyPair.getOriginalPublicKey());
+            Boolean verify = Ed25519.verify(originalPublicKeyBytes,dataBytes,signatureBytes);
+            return verify;
+        }
+        return false;
+    }
+
+    public static boolean verify(String publicKey,byte[] data,byte[] signature){
+        XenonKeyPair xenonKeyPair=convertorPublicKey(publicKey);
+        Network network=xenonKeyPair.getNetwork();
+        Algorithm algorithm=xenonKeyPair.getAlgorithm();
+
+        if(algorithm.equals(Algorithm.ED25519)){
+            byte[] originalPublicKeyBytes=Base58.decode(xenonKeyPair.getOriginalPublicKey());
+            Boolean verify = Ed25519.verify(originalPublicKeyBytes,data,signature);
+            return verify;
+        }
+        return false;
+    }
+
+
+
+    private static XenonKeyPair convertorPrivateKey(String privateKey){
+        XenonKeyPair  xenonKeyPair=new XenonKeyPair();
+        byte[] privateKeyBytes= Base58.decode(privateKey);
+        xenonKeyPair.setNetwork(Network.findByCode(privateKeyBytes[0]));
+        xenonKeyPair.setAlgorithm(Algorithm.findByCode(privateKeyBytes[1]));
+        xenonKeyPair.setPrivateKey(privateKey);
+
+        byte[] originalPrivateKeyBytes=Arrays.copyOfRange(privateKeyBytes,2,privateKeyBytes.length);
+        xenonKeyPair.setOriginalPrivateKey(Base58.encode(originalPrivateKeyBytes));
+        return xenonKeyPair;
+    }
+
+    private static XenonKeyPair convertorPublicKey(String publicKey){
+        XenonKeyPair  xenonKeyPair=new XenonKeyPair();
+        byte[] publicKeyBytes= Base58.decode(publicKey);
+        xenonKeyPair.setNetwork(Network.findByCode(publicKeyBytes[0]));
+        xenonKeyPair.setAlgorithm(Algorithm.findByCode(publicKeyBytes[1]));
+        xenonKeyPair.setPublicKey(publicKey);
+
+        byte[] originalPrivateBytes=Arrays.copyOfRange(publicKeyBytes,2,publicKeyBytes.length);
+        xenonKeyPair.setOriginalPublicKey(Base58.encode(originalPrivateBytes));
+        return xenonKeyPair;
+    }
+    public static byte[] doECDH (String privateKeyStr, String publicKeyStr)
+    {
+        Security.addProvider(new BouncyCastleProvider());
+        try{
+            PrivateKey prvk = Keccak256Secp256k1.getPrivateKey(Base58.decode(privateKeyStr));
+            PublicKey pubk = Keccak256Secp256k1.getPublicKey(Base58.decode(publicKeyStr));
+
+            KeyAgreement keyAgree = KeyAgreement.getInstance("ECDH", "BC");
+            keyAgree.init(prvk);
+            keyAgree.doPhase(pubk, true);
+            byte[] secret = keyAgree.generateSecret();
+            return secret;
+        }catch (Exception e){
+            log.error("doECDH",e);
+        }
+        return null;
+    }
+
+}
